3aeaab32221c680b2c40e7050a1444db2779e7fc
jonkenobi <eragon.pokemon@hotmail.com>
2023-07-17 17:22:31 +0900

    Add Search function Filters in the FE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,23 @@ import { MatListModule } from '@angular/material/list';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
+import {MatSelectModule}  from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
 import { MapComponent } from './map/map.component';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientJsonpModule } from '@angular/common/http';
 import { AreaExpansionPanelComponent } from './components/area-expansion-panel/area-expansion-panel.component';
+import { SearchInputAreaComponent } from './components/search-input-area/search-input-area.component';
 
 @NgModule({
-  declarations: [AppComponent, MapComponent, FeedbackComponent, AreaExpansionPanelComponent],
+  declarations: [AppComponent, MapComponent, FeedbackComponent, AreaExpansionPanelComponent,SearchInputAreaComponent ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -22,8 +26,11 @@ import { AreaExpansionPanelComponent } from './components/area-expansion-panel/a
     HttpClientModule,
     NoopAnimationsModule,
     HttpClientJsonpModule,
+    MatButtonModule,
+    MatCheckboxModule,
     MatCardModule,
     MatIconModule,
+    MatSelectModule,
     MatExpansionModule
   ],
   providers: [],
---
+++ src/app/components/search-input-area/search-input-area.component.ts
@@ -0,0 +1,50 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-search-input-area',
+  templateUrl: './search-input-area.component.html',
+  styleUrls: ['./search-input-area.component.css'],
+})
+export class SearchInputAreaComponent implements OnInit {
+  @Output() onSearch = new EventEmitter<Object>();
+  tripDuration = 1;
+  attributes = new Set();
+
+  onSubmit() {
+    this.onSearch.emit({
+      tripDuration: this.tripDuration,
+      attributes: this.attributes,
+    });
+  }
+
+  attributesOptions = [
+    { name: 'shopping', value: true, icon: 'shopping_bag' },
+    { name: 'drinks', value: true, icon: 'local_bar' },
+    { name: 'coffee', value: true, icon: 'coffee' },
+    { name: 'nature', value: true, icon: 'park' },
+  ];
+  tripDurations = [
+    { name: 'Closeby (~20 mins)', value: 1 },
+    { name: '20 ~ 40 mins', value: 2 },
+    { name: 'Up to 1 hr', value: 3 },
+  ];
+  // timeDurations = [
+  //   { name: '~3hrs', value: 1 },
+  //   { name: 'Half Day (3~5 hrs)', value: 2 },
+  //   { name: '1 day', value: 3 },
+  // ];
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onCheckboxChange(event: any, propertyName: string) {
+    event.checked
+      ? this.attributes.add(propertyName)
+      : this.attributes.delete(propertyName);
+  }
+
+  capitalizeFirstLetter(string: String) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+}
